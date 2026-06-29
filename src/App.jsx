5d37948121693e613003f9c797c3c37c09100baf
eileendsam <eileendsam@outlook.com>
2026-06-29 19:49:16 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { PlayProvider } from '@playhtml/react'
+
+function App() {
+  return (
+    <PlayProvider>
+      <h1>digital graveyard</h1>
+    </PlayProvider>
+  )
+}
+
+export default App
